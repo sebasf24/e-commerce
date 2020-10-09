40beef164bebe90c7378f2cdeadd86366a8e8110
sebasf24 <sebastianferreyra@gmail.com>
2020-10-09 11:50:20 -0300

    add redux-hooks and connect bd with front

--- client/src/Component/Product/Product.jsx
+++ client/src/Component/Product/Product.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { Link } from 'react-router-dom';
-import data from '../../data.js';
+import { useDispatch, useSelector, useStore } from 'react-redux';
+import {mostrarProducto_id} from "../../actions/products.js"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Image, Col, Row, Container, Button } from 'react-bootstrap';
 
 export default function Product(props) {
     const id = props.match.params.id;
-    const product = data[id];
-    console.log(product);
-    const { name, description, price, stock, img } = product;
+
+    const productS = useSelector(state=>state.products);
+    
+    const {selectedProduct}=productS;
+    const { name, description, price, stock, img }=selectedProduct;
+
+    const dispatch=useDispatch();
+  useEffect(()=>{
+
+    dispatch(mostrarProducto_id(id))
+    return ()=>{
+
+    }
+  },[])
+  
 
     return (
         <Container style={{ width: '70rem' }}>
             <Card style={{ border: 'none' }}>
                 <Row >
-                    {console.log(name)}
                     <Col>
                         <Image style={{ width: '25rem', height: '15rem' }} src={img} rounded />
                     </Col>
