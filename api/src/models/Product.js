9d9c209b85cd5404a7eb40b47c16b25888b6b1e2
MBetinotti <mbetinotti@gmail.com>
2020-10-10 13:31:48 -0300

    navbar y estilos

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -20,7 +20,7 @@ module.exports = (sequelize) => {
       allowNull: false
     },
     img: {
-      type: DataTypes.BLOB
+      type: DataTypes.STRING
     }
   });
 };
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React,{useEffect} from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import FormCategory from './Component/FormCategory/FormCategory.jsx'
 import Product from './Component/Product/Product.jsx'
 import Catalogue from './Component/Catalogue/Catalogue.jsx'
 import { useDispatch, useSelector, useStore } from 'react-redux';
 import {mostrarProductos} from "../src/actions/products.js"
+import NavBar from './Component/NavBar/navbar.jsx';
 
 
 function App() {  
@@ -24,13 +25,15 @@ function App() {
   },[])
  
   return (
-    <div>
+  
+    <Router forceRefresh>
      
+    <Route path='/' component={NavBar}/>
     <Route exact path='/products' render={()=> <Catalogue Products={products}/>} />
     <Route path='/products/:id' component={Product}/>
     <Route path='/addCategory' component={FormCategory}/>
     
-    </div>
+    </Router>
   );
 }
 
