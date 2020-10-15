9e368779643cbeef7b068aea4327c614928ca87f
sebasf24 <sebastianferreyra@gmail.com>
2020-10-14 22:56:18 -0300

    add item cart in user order and  orders route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,14 +4,17 @@ const productRouter = require('./product.js');
 const categoryRouter = require('./category.js');
 const searchRouter = require('./search.js');
 const userRouter= require('./user.js');
+const orderRouter =require('./order.js');
 
 const router = Router();
 
 // load each router on a route
 // i.e: router.use('/auth', authRouter);
 // router.use('/auth', authRouter);
+router.use('/orders', orderRouter);
 router.use('/products', productRouter);
 router.use('/category', categoryRouter);
 router.use('/search', searchRouter);
 router.use('/user', userRouter);
+
 module.exports = router;
