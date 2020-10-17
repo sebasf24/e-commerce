bfae8b99e5af9c3e2033cd2b16ea13a780cb24d9
luciogayoso <luciogayoso@gmail.com>
2020-10-17 14:24:14 -0300

    vaciar carrito

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -1,130 +1,159 @@
 const server = require('express').Router();
-const { User, Order,Order_line, Product }=require('../db');
+const { User, Order, Order_line, Product } = require('../db');
 
 //Ruta que retorne todos los Usuarios
-server.get('/',(req, res)=>{
+server.get('/', (req, res) => {
     User.findAll()
-    .then(user=>
-        res.send(user))
+        .then(user =>
+            res.send(user))
 
 })
 //Ruta que retorne todas las Ordenes de los usuarios
-server.get('/:id/orders',(req, res)=>{
-    const {id}= req.params;
+server.get('/:id/orders', (req, res) => {
+    const { id } = req.params;
     Order.findAll(
-       { where:{
-        userId:id
-        }}
+        {
+            where: {
+                userId: id
+            }
+        }
     )
-    .then(orden=>
-        res.send(orden))
+        .then(orden =>
+            res.send(orden))
 
 })
 
 //Ruta para creación de Usuario
-server.post('/',(req, res)=>{
+server.post('/', (req, res) => {
     User.create({
-        name:req.body.name,
-        lastname:req.body.lastname,
-        dni:req.body.dni,
-        email:req.body.email,
-        username:req.body.username,
-        password:req.body.password,
-        image:req.body.image,
-        typeUser:req.body.typeUser,
+        name: req.body.name,
+        lastname: req.body.lastname,
+        dni: req.body.dni,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+        image: req.body.image,
+        typeUser: req.body.typeUser,
     })
-    .then((user)=>{
-        res.send(user)
-    })
-    .catch(error=>{
-        res.status(500).send("Error: "+ error)
-    })
+        .then((user) => {
+            res.send(user)
+        })
+        .catch(error => {
+            res.status(500).send("Error: " + error)
+        })
 
 })
 //Ruta para modificar Usuario
-server.put('/:id',(req, res)=>{
-    const {id}= req.params;
+server.put('/:id', (req, res) => {
+    const { id } = req.params;
     User.update({
-        name:req.body.name,
-        lastname:req.body.lastname,
-        dni:req.body.dni,
-        email:req.body.email,
-        username:req.body.username,
-        password:req.body.password,
-        image:req.body.image,
-        typeUser:req.body.typeUser,
+        name: req.body.name,
+        lastname: req.body.lastname,
+        dni: req.body.dni,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password,
+        image: req.body.image,
+        typeUser: req.body.typeUser,
     },
-    {
-        where:{id}
-    })
-    .then((user)=>{
-        res.send(user)
-    })
-    .catch(error=>{
-        res.status(500).send("Error: "+ error)
-    })
+        {
+            where: { id }
+        })
+        .then((user) => {
+            res.send(user)
+        })
+        .catch(error => {
+            res.status(500).send("Error: " + error)
+        })
 
 })
 //ruta que devuelve todos los items del carrito
-server.get('/:idUser/cart', (req,res)=>{
-    const {idUser} = req.params;
+server.get('/:idUser/cart', (req, res) => {
+    const { idUser } = req.params;
 
     Product.findAll({
-        attributes: ['id','name'],
-		include:[{
-			model:Order,
-			attributes: ['id','estado'],
-			where:{
-				userId:idUser
-			},
-			through: {attributes: ["cantidad"]}
-		}]
-	})
-        .then(respuesta=>{
-        return res.send(respuesta)
+        attributes: ['id', 'name'],
+        include: [{
+            model: Order,
+            attributes: ['id', 'estado'],
+            where: {
+                userId: idUser
+            },
+            through: { attributes: ["cantidad"] }
+        }]
     })
+        .then(respuesta => {
+            return res.send(respuesta)
+        })
 
-    
+
 })
 //ruta que agrega un item al carrito
 server.post('/:idUser/cart', (req, res) => {
-    const {idUser} = req.params;
-    const {estado,productId,cantidad,price}=req.body
+    const { idUser } = req.params;
+    const { estado, productId, cantidad, price } = req.body
+    console.log(req.body);
 
-
-     Order.findOrCreate({
-       where:{userId:idUser},
-       defaults: {estado:estado,userId:idUser}
-    }).then(respuesta=>{
-
+    Order.findOrCreate({
+        where: {
+            userId: idUser
+        },
+        defaults: {
+            estado: estado,
+            userId: idUser
+        }
+    }).then(respuesta => {
         Order_line.create({
-            orderId:respuesta[0].id,
-            productId:productId,
-            cantidad:cantidad,
+            orderId: respuesta[0].id,
+            productId: productId,
+            cantidad: cantidad,
             price: price
         })
-
-        return res.status(200).send(respuesta);
-    }).catch(err=>{
-        return res.send(err);
+        .then(order => {
+            return res.status(200).send(order);
+        })
+    }).catch(err => {
+        console.log('estos es un error ' + err);
+        return res.send('estos es un error ' + err);
     })
+});
 
+server.delete('/:idUser/cart', (req, res) => {
+    const { idUser } = req.params;
+
+    Order.destroy({
+        where: {
+            userId: idUser
+        }
+    })
+    .then(respuesta => {
+        return res.send('Se vacio el carrito')
+    })
+    .catch(err => {
+        res.send("algo salio mal")
+    })
 
 });
 
+//PUT /users/:idUser/cart
+
+
 //Ruta para eliminar un usuario
 server.delete('/:id', (req, res) => {
-    const {id} = req.params;
-    User.destroy({ where: {id} })
-    .then(user=>{
-        if (!user){
-            //sino lo encuentra un usuario devuelve un error
-            return res.status(400).send("El usuario no existe");
-        } else {
-            //sino, borra usuario
-            return res.status(200).send("El usuario fue eliminado")
-        }
-    })
+    const { id } = req.params;
+    User.destroy({ where: { id } })
+        .then(user => {
+            if (!user) {
+                //sino lo encuentra un usuario devuelve un error
+                return res.status(400).send("El usuario no existe");
+            } else {
+                //sino, borra usuario
+                return res.status(200).send("El usuario fue eliminado")
+            }
+        })
+        .catch(err => {
+            res.send('Esto es un error: ' + err);
+        })
 });
 
 
