98a3a89bbd00bd8b2b19347ca205d7f646f1d78c
delfinalago <delfinalago@gmail.com>
2020-10-18 00:17:20 -0300

    ORDER

--- client/src/Component/Order/Order.jsx
+++ client/src/Component/Order/Order.jsx
@@ -1,38 +1,25 @@
 import React,{useEffect} from 'react';
 import {Container,Card,Button,Navbar,Nav} from 'react-bootstrap';
-// import ItemCart from './ItemCart.jsx';
 import OrderLine from './OrderLine.jsx';
 import styles from './Order.module.css';
-// 
-import {agregarProductoCarrito,vaciarCarrito} from "../../actions/cart";
 import { useDispatch, useSelector, useStore } from 'react-redux';
 
 export default function Order(){
     console.log("COMPONENTE ORDEN");
 const dispatch = useDispatch();
-//const products = useSelector(store=>store.productsCart)
-//let productosCarrito = products.productos
-//obtengo los productos de localStorage
-if(!localStorage.carritoLocal){
-    localStorage.setItem("carritoLocal",JSON.stringify([]))
-}
+
 var prodGuardados = JSON.parse(localStorage.getItem("carritoLocal"))
 console.log("PRODUCTOS GUARDADOS = ", prodGuardados);
-// const vaciar=()=>{
-//     dispatch(vaciarCarrito())
-//     localStorage.setItem("carritoLocal",JSON.stringify([]))
-// }
+
 
     return (
         <Container className={styles.container}>
-            
             <Container className={styles.list}>
                 <Navbar className={styles.heaedr}>
                     <Navbar.Collapse className="justify-content-start">
                         <Navbar.Brand> ORDER ! </Navbar.Brand>
                     </Navbar.Collapse>
                     <Nav className="mr-auto">
-                    {/* <Button  onClick={vaciar} variant="outline-danger">VACIAR CARRITO</Button> */}
                     </Nav>
                 </Navbar>
                 <Container className={styles.lista}>
@@ -78,13 +65,7 @@ console.log("PRODUCTOS GUARDADOS = ", prodGuardados);
                             0
                         }
                     </p>
-                    </div>
-                    
-                    {/* <Card.Footer className={styles.boton}>
-                        <Button className={styles.botonCancelar+' '+styles.button}>Cancelar</Button>
-                        <Button className={styles.botonAceptar+' '+styles.button}>Finalizar Compra</Button>
-                    </Card.Footer> */}
-                    
+                    </div> 
                 </Card>
                 </Container>
             </Container>
@@ -101,46 +82,3 @@ console.log("PRODUCTOS GUARDADOS = ", prodGuardados);
 } 
 
 
-
-// //COMPONENTE ORDER
-// export class Order extends Component {
-//     constructor(props){
-//       super(props)
-//       this.state = {
-//         products:[]
-//       }
-//     }
-
-
-
-//     //TRAE EL CARRITO DE UN USUARIO.. LO PRECARGA Y LO DEJA EN EL STORE DE REDUX
-//   componentDidMount(){
-//     if (this.props.user.id != 0){
-//       this.props.getProductsCart(this.props.user.id);
-//     }
-//     this.props.setRedirect(true)
-//   }
-
-//   componentWillUnmount(){
-//     this.props.setRedirectOff()
-//   }
-
-
-//     //FUNCION QUE CALCULA EL TOTAL DE LA COMPRA
-//   calculoTotal (products) {
-//     var totalDeOrden = 0;
-//     if (this.props.user.id !=0){
-//       products.map( e => {
-//         totalDeOrden = totalDeOrden + (e.order_line.price * e.order_line.cantidad)
-//       })
-//       return totalDeOrden;
-//     } else {
-//       products.map( e =>{
-//         totalDeOrden = totalDeOrden + (Number(e.price) * Number(e.cantidad));
-//       })
-//       return totalDeOrden;
-//     }
-//   }
-
-//     /*TOTAL DEL COSTO DE UN PRODUCTO */
-//   <h5> Total $ {el.cantidad*Number(el.price)}</h5>
