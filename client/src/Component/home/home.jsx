2df1207c2863404cd39a7ed5568d2f829b44c653
MBetinotti <mbetinotti@gmail.com>
2020-10-21 21:14:04 -0300

    home

--- client/src/Component/home/home.jsx
+++ client/src/Component/home/home.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../NavBar/technav1.png';
+import Fondo from './fondo.jpg';
 import style from './home.css'
-import {Button} from 'react-bootstrap';
-
+import { Card, Image, Col, Row, Container, Button,Form } from 'react-bootstrap';
 export default function Home(){
     return (
-        <div>
-            <img src={Logo} className={style.img}/>
+        <Container className={style.container}>
+            <Image src={Fondo} className={style.img}/>
             <p>EN CONTRUCCION!!!!</p>
             <Link to="/products">
                 <Button type="button" class="btn btn-primary btn-lg" >
@@ -15,6 +14,6 @@ export default function Home(){
                         <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/></svg>
                 </Button>
             </Link> 
-        </div>
+        </Container>
     )
 }
