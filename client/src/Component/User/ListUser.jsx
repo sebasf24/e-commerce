3b9e1d5aefe4e07fd38ae2b64e323bca5c2af36d
LuchiArq <lucianoarquiel@gmail.com>
2020-10-26 08:57:28 -0300

    asda

--- client/src/Component/User/ListUser.jsx
+++ client/src/Component/User/ListUser.jsx
@@ -90,7 +90,7 @@ export default function ListUser() {
                     </thead>
                     <tbody>
                         {
-                            listaUsuarios ? listaUsuarios.map(us => {
+                            users ? users.map(us => {
                                 return (
                                     <tr>
                                         <td><InputGroup.Checkbox className="mb-3" /></td>
