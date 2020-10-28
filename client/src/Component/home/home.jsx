4fbac5aa5dc13b2c4ed3c16ad929979910218f50
MBetinotti <mbetinotti@gmail.com>
2020-10-28 10:08:28 -0300

    logitos

--- client/src/Component/home/home.jsx
+++ client/src/Component/home/home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Carrusel from './Carrusel'
 import Homecategory from './Homcategory'
+import camion from './camion.png';
+import mercado from './mercado.png';
+import gps from './gps.png'
 
 export default function Home(){
   return (
@@ -10,25 +13,44 @@ export default function Home(){
   <div class="card">
    
     <div class="card-body">
-      <h5 class="card-title">Metodos de Pago</h5>
-      <p class="card-text">Aca podria poner unas imagens de las tarjetas supongo</p>
+    <div class="row">
+        <div class="col">
+          <img src={mercado} alt="img" style={{height: 110 , width:120, marginLeft: 25}}/>
+        </div>
+        <div class="col">
+      <h4 class="card-title">Metodos de Pago</h4>
+      <p class="card-text">Todas las tarjetas.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    
+    <div class="card-body">
+      <div class="row">
+        <div class="col">
+          <img src={camion} alt="img" style={{height: 70 , width:110, marginLeft: 25, marginTop:20  }}/>
+        </div>
+        <div class="col">
+      <h4 class="card-title" style={{marginTop:20}}>Envio Gratis</h4>
+      <p class="card-text">en todos los productos</p>
+        </div>
+      </div>
       
     </div>
   </div>
   <div class="card">
     
     <div class="card-body">
-      <h5 class="card-title">Envio Gratis</h5>
-      <p class="card-text">dibujito de un camioncito?</p>
-      
-    </div>
-  </div>
-  <div class="card">
-    
-    <div class="card-body">
-      <h5 class="card-title">No se me ocurre nada</h5>
-      <p class="card-text">blabla bla bla blablabla bla bla</p>
-      
+    <div class="row">
+        <div class="col">
+          <img src={gps} alt="img" style={{height: 70 , width:110, marginLeft: 25, marginTop:20  }}/>
+        </div>
+        <div class="col">
+      <h4 class="card-title" style={{marginTop:20}}>Envios a todo el pais!</h4>
+   
+        </div>
+      </div>
     </div>
   </div>
 </div>
